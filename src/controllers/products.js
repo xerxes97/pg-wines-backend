fd59481f9c1b59edf27f3d93596c13a25e6bda21
almada gaspar <almada3d@gmail.com>
2021-09-03 18:52:23 -0300

    add routes GET products by name

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,19 +1,60 @@
 const { Product, Category } = require('../db');
+const { Op } = require("sequelize");
 
 async function getProducts(req, res) {
+    const { name } = req.query;
     try {
-        const products = await Product.findAll({
-            // include: {
-            //     model: Category,
-            //     attributes: ['name'],
-            //     through: { attributes: [] }
-            // }
-        })
-        return res.send(products);
+        if (name) {
+            let products = await Product.findAll({
+                where: {
+                    name: { [Op.iLike]: `%${name}%` }
+                },
+                include: ["category"],
+            })
+
+            products = products.map(elem => {
+                return {
+                    id: elem.id,
+                    name: elem.name,
+                    // stock: elem.stock,
+                    cost: elem.cost,
+                    // description: elem.description,
+                    discount: elem.discount,
+                    // capacity: elem.capacity,
+                    image: elem.image,
+                    // sales: elem.sales,
+                    category: elem.category.name
+                }
+            });
+                        
+            if (products.length) return res.send(products);
+            return res.status(404).send('Product not found');
+
+        } else {
+            let products = await Product.findAll({
+                include: ["category"],
+            })
+            products = products.map(elem => {
+                return {
+                    id: elem.id,
+                    name: elem.name,
+                    // stock: elem.stock,
+                    cost: elem.cost,
+                    // description: elem.description,
+                    discount: elem.discount,
+                    // capacity: elem.capacity,
+                    image: elem.image,
+                    // sales: elem.sales,
+                    category: elem.category.name
+                }
+            });
+            return res.send(products);
+        }
     } catch (err) {
-        console.log('ERROR en getProducts', err);
+        console.log('ERROR in getProducts', err);
     }
 }
+
 
 
 
