6c332ea794ac4e277f707f497b4c4fe19bb170bd
Gustavitory <78607300+Gustavitory@users.noreply.github.com>
2021-09-04 21:50:52 -0400

    Routes_Update-Delete-Product

--- src/routes/products/index.js
+++ src/routes/products/index.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const router = Router();
-const { getProducts, getProductById, postProduct } = require('../../controllers/products')
+const { getProducts, getProductById, postProduct, updateProduct, deleteProduct } = require('../../controllers/products')
 const { getCategories, postCategory } = require('../../controllers/categories')
 
 router.get('/products', getProducts);
@@ -8,5 +8,7 @@ router.get('/categories', getCategories);
 router.get('/product/:id', getProductById);
 router.post('/category', postCategory);
 router.post('/product', postProduct);
+router.put('/product/update',updateProduct);
+router.delete('/product/delete',deleteProduct);
 
 module.exports = router;
