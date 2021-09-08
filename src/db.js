d0a47ae70e665c108d90c1450ec7157ac1e728e4
Gustavitory <78607300+Gustavitory@users.noreply.github.com>
2021-09-08 02:17:35 -0400

    Routes_Get-Products-100%

--- src/db.js
+++ src/db.js
@@ -35,7 +35,6 @@ fs.readdirSync(path.join(__dirname, '/models'))
   .forEach((file) => {
     modelDefiners.push(require(path.join(__dirname, '/models', file)));
   });
-
 // Injectamos la conexion (sequelize) a todos los modelos
 modelDefiners.forEach(model => model(sequelize));
 // Capitalizamos los nombres de los modelos ie: product => Product
@@ -50,10 +49,7 @@ const { Product, Category, Brands } = sequelize.models;
 // Aca vendrian las relaciones
 
 Category.hasMany(Product);
-Product.belongsTo(Category, {
-  foreignKey: "categoryId",
-  as: "category",
-});
+Product.belongsTo(Category);
 
 Brands.hasMany(Product);
 Product.belongsTo(Brands, {
