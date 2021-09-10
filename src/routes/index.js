2f7ea1aa7f334f8b408778cee2777e5a2fec2216
Gustavitory <78607300+Gustavitory@users.noreply.github.com>
2021-09-10 03:55:50 -0400

    Controls_user2

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ const products = require('./products');
 const categories = require('./categories');
 const brands = require('./brands');
 const packing = require('./packing');
+const user= require('./user/user');
 
 const router = Router();
 
@@ -11,6 +12,7 @@ router.use('/products', products);
 router.use('/categories', categories);
 router.use('/brands', brands);
 router.use('/packing', packing);
+router.use('/user',user);
 
 router.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, '../../client/index.html'));
