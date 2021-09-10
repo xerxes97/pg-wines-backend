db02a895aa4ab67380d9b10d9f0f9f9161185f21
Alex Marin Mendez <alexmarinmendez@gmail.com>
2021-09-10 01:11:58 -0500

    feat: add Offer Model to make offer slider run on the frontend

---
+++ src/controllers/offers.js
@@ -0,0 +1,73 @@
+const { Offer } = require('../db');
+
+async function getOffers(req, res) {
+    try {
+        const offers = await Offer.findAll()
+        return res.send(offers);
+    } catch (err) {
+        console.log('ERROR in getOffers', err);
+    }
+}
+
+async function postOffer(req, res) {
+    const { status, slug, productId } = req.body;
+    const image = req.file? req.file.filename : undefined;
+    try {
+        if (status && image && productId) {
+            const createdOffer = await Offer.create({
+                status,
+                image,
+                slug
+            });
+            createdOffer.setProduct(productId);
+            res.send(createdOffer);
+        } else {
+            res.status(422).send({ error: 'Fields status, image and productId are required' })
+        }
+    } catch (err) {
+        console.log('ERROR in postOffer', err);
+    }
+}
+
+async function updateOffer(req, res) {
+    const { id, status, slug, productId } = req.body;
+    const image = req.file? req.file.filename : undefined;
+    if (!id) return res.status(422).send({ error: 'The offer id is required' });
+
+    try {
+        const offer = await Offer.findByPk(id);
+        if (!offer) return res.status(422).send({ error: 'The offer id is wrong' });
+        status ? offer.status = status : offer.status = offer.status;
+        image ? offer.image = image : offer.image = offer.image;
+        slug ? offer.slug = slug : offer.slug = offer.slug;
+        productId ? offer.productId = productId : offer.productId = offer.productId;
+        await offer.save();
+        return res.send('The offer has been updated suscesfully');
+    } catch (err) {
+        console.log('ERROR in updateOffer', err);
+    }
+}
+
+async function deleteOffer(req, res) {
+    const { id } = req.body;
+    if (!id) return res.send({ error: 'The offer id is required' })
+    const offer = await Offer.findByPk(id)
+    if (!offer) return res.send({ error: 'There is not any offer with this id' })
+    try {
+        await Offer.destroy({
+            where: {
+                id
+            }
+        })
+        return res.send('The offer was removed successfully')
+    } catch (err) {
+        console.log('ERROR in deleteOffer', err);
+    }
+}
+
+module.exports = {
+    getOffers,
+    postOffer,
+    updateOffer,
+    deleteOffer
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ const products = require('./products');
 const categories = require('./categories');
 const brands = require('./brands');
 const packing = require('./packing');
+const offers = require('./offers');
 
 const router = Router();
 
@@ -11,6 +12,7 @@ router.use('/products', products);
 router.use('/categories', categories);
 router.use('/brands', brands);
 router.use('/packing', packing);
+router.use('/offers', offers);
 
 router.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, '../../client/index.html'));
