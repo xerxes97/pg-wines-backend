7d54d9b2f0c6fb4682695dbb24b1cb4155494a47
emilianoduartes <emilianoduartes@gmail.com>
2021-09-14 19:21:10 -0300

    new deploy

--- src/models/Order.js
+++ src/models/Order.js
@@ -2,15 +2,15 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
     sequelize.define('order', {    
-      status: {
-        type: DataTypes.ENUM('approved', 'rejected', 'cart'),
-        defaultValue: 'cart',
-        allowNull: false,
-      },
-      shippingStatus: {
-        type: DataTypes.ENUM('uninitiated', 'processing', 'approved', 'cancelled'),
-        defaultValue: 'uninitiated',
-        allowNull: false,
-      },
+      // status: {
+      //   type: DataTypes.ENUM('approved', 'rejected', 'cart'),
+      //   defaultValue: 'cart',
+      //   allowNull: false,
+      // },
+      // shippingStatus: {
+      //   type: DataTypes.ENUM('uninitiated', 'processing', 'approved', 'cancelled'),
+      //   defaultValue: 'uninitiated',
+      //   allowNull: false,
+      // },
     });
   };
