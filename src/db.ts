aae4ad7b5a20e44577497cdb1221c2cc813846d1
cristianQuev <119689796+cristianQuev@users.noreply.github.com>
2023-01-15 17:51:34 -0500

    relations

--- src/db.ts
+++ src/db.ts
@@ -5,23 +5,10 @@ const config = require(__dirname + '/config/config.ts').dev;
 const Sequelize = require('sequelize');
 const db: any = {};
 
-  // const connectionString: Options = {
-  //   logging: true,
-  //   native: true,
-  //   dialect: "postgres",
-  //   database: DB_NAME,
-  //   username: DB_USER,
-  //   password: DB_PASSWORD,
-  //   host: DB_HOST
-  // }
-
 const sequelize = new Sequelize(config);
 const basename = path.basename(__filename);
 
-// const modelDefiners: any = [];
-
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefine
-// TODO: change extension from js to ts
 fs.readdirSync(path.join(__dirname, '/models'))
   .filter((file: string) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.ts'))
   .forEach((file: any) => {
@@ -35,8 +22,6 @@ Object.keys(db).forEach(modelName => {
     db[modelName].associate(db);
   }
 });
-const z = sequelize.models;
-console.log("**********",z)
 // Capitalizamos los nombres de los modelos ie: product => Product
 // let entries = Object.entries(sequelize.models);
 // let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
@@ -44,32 +29,32 @@ console.log("**********",z)
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-// const { Product, Category, Brand, Packing, Offer, Review, User } = sequelize.models;
+const { Product, Category, Brand, Packing, Offer, Review, User } = sequelize.models;
 
 // Aca vendrian las relaciones
 
-// Category.hasMany(Product);
-// Product.belongsTo(Category);
+Category.hasMany(Product);
+Product.belongsTo(Category);
 
-// Brand.hasMany(Product);
-// Product.belongsTo(Brand, {
-//   foreignKey: "brandId"
-// });
+Brand.hasMany(Product);
+Product.belongsTo(Brand, {
+  foreignKey: "brandId"
+});
 
-// Packing.hasMany(Product);
-// Product.belongsTo(Packing, {
-//   foreignKey: "packingId",
-//   as: "packing",
-// });
+Packing.hasMany(Product);
+Product.belongsTo(Packing, {
+  foreignKey: "packingId",
+  as: "packing",
+});
 
-// Product.hasOne(Offer);
-// Offer.belongsTo(Product);
+Product.hasOne(Offer);
+Offer.belongsTo(Product);
 
-// Product.hasMany(Review);
-// Review.belongsTo(Product);
+Product.hasMany(Review);
+Review.belongsTo(Product);
 
-// User.hasMany(Review);
-// Review.belongsTo(User);
+User.hasMany(Review);
+Review.belongsTo(User);
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
